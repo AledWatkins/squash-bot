006d6a9d57580da1b5e036615cf904ea37e12b44
Aled Watkins <aled.watkins2@gmail.com>
2024-03-23 20:48:40 +0000

    Add `CommandOption`

--- squash_bot/core/command.py
+++ squash_bot/core/command.py
@@ -6,9 +6,17 @@ from squash_bot.core import response
 T_command_context = typing.TypeVar("T_command_context", bound=attrs.AttrsInstance)
 
 
+class CommandOption:
+    name: str
+    description: str
+    type: str
+    required: bool
+
+
 class Command(typing.Generic[T_command_context]):
     name: str
     description: str
+    options: list[CommandOption]
 
     def __init__(self) -> None: ...
 
