cab37076cad7ad6484294cbbc07d126d1e7d6a33
Aled Watkins <aled.watkins2@gmail.com>
2024-03-23 18:34:08 +0000

    Add `InteractionResponseType` and use in ping response

--- squash_bot/core/lambda_function.py
+++ squash_bot/core/lambda_function.py
@@ -15,6 +15,11 @@ class InteractionTypeEnum(enum.Enum):
         return cls.UNKNOWN
 
 
+class InteractionResponseType(enum.Enum):
+    PONG = 1
+    CHANNEL_MESSAGE_WITH_SOURCE = 4
+
+
 def lambda_handler(
     event: dict[str, typing.Any], context: dict["str", typing.Any]
 ) -> dict[str, typing.Any]:
@@ -33,7 +38,7 @@ def lambda_handler(
 def ping_handler(body: dict[str, typing.Any]) -> response.Response:
     return response.Response(
         status_code=200,
-        body_data={"type": InteractionTypeEnum.PING.value},
+        body_data={"type": InteractionResponseType.PONG.value},
     )
 
 
