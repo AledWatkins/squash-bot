2fef2633c30194644a79c6d57b8938f8f69038d4
Aled Watkins <aled.watkins2@gmail.com>
2024-03-23 21:39:09 +0000

    Add `match_tracker` commands to localdev `installed_commands`

--- squash_bot/settings/localdev.py
+++ squash_bot/settings/localdev.py
@@ -6,6 +6,10 @@ class Settings(base.BaseSettings):
     Settings class for localdev and testing
     """
 
+    installed_commands = [
+        "squash_bot.match_tracker.commands",
+    ]
+
     APP_ID = "[APP_ID]"
     SERVER_ID = "[SERVER_ID]"
     BOT_TOKEN = "[BOT_TOKEN]"
