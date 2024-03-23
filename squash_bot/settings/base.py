86a39292b4a3397f0c68819ada588dccb400c294
Aled Watkins <aled.watkins2@gmail.com>
2024-03-23 18:08:12 +0000

    Add `get_class_from_string` helper function
    This gives us a way to specify classes as opposed to just modules in
    settings.

--- squash_bot/settings/base.py
+++ squash_bot/settings/base.py
@@ -10,3 +10,9 @@ class BaseSettings:
 
 settings_module = os.environ["SETTINGS_MODULE"]
 settings = importlib.import_module(settings_module).Settings()
+
+
+def get_class_from_string(class_path: str) -> type[object]:
+    module_name, class_name = class_path.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    return getattr(module, class_name)
