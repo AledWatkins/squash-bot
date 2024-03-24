b93cc948b29a047c3ac0d8e6de0a03fda546f412
Aled Watkins <aled.watkins2@gmail.com>
2024-03-24 14:52:34 +0000

    Fix tests after body handling change

--- tests/core/test_lambda_function.py
+++ tests/core/test_lambda_function.py
@@ -7,7 +7,7 @@ class TestLambdaHandler:
     def test_ping_response(self):
         response = lambda_function.lambda_handler(
             {
-                "body": json.dumps({"type": 1}),
+                "body": {"type": 1},
             },
             {},
         )
@@ -19,11 +19,11 @@ class TestLambdaHandler:
     def test_unknown_type_response(self):
         response = lambda_function.lambda_handler(
             {
-                "body": json.dumps({"type": 31}),
+                "body": {"type": 31},
             },
             {},
         )
         assert response == {
             "statusCode": 400,
-            "body": '"Unknown interaction type"',
+            "body": json.dumps("Unknown interaction type"),
         }
