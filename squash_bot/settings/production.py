9cb9d71ccf6cc9aeb33aace773974cca00e85c2e
Aled Watkins <aled.watkins2@gmail.com>
2024-03-24 17:52:19 +0000

    Remove unneeded settings from prod settings

--- squash_bot/settings/production.py
+++ squash_bot/settings/production.py
@@ -12,9 +12,5 @@ class Settings(base.BaseSettings):
         "squash_bot.match_tracker.commands",
     ]
 
-    APP_ID = os.environ.get("APP_ID")
-    SERVER_ID = os.environ.get("SERVER_ID")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
     OUTPUTER = "squash_bot.slash_command_register.outputer.RequestsOutputer"
     VERIFYIER = "squash_bot.core.verify.NACLVerifier"
