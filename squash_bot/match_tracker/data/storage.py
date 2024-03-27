39f43d4c8bb1d7bcaf38da800c26891cc91c8a3b
Aled Watkins <aled.watkins2@gmail.com>
2024-03-27 22:20:53 +0000

    Add `store_match_result` function

--- squash_bot/match_tracker/data/storage.py
+++ squash_bot/match_tracker/data/storage.py
@@ -17,3 +17,22 @@ def get_all_match_results() -> list[dataclasses.MatchResult]:
     return [
         dataclasses.MatchResult.from_dict(result) for result in get_all_match_results_as_dict()
     ]
+
+
+def convert_match_results_to_dicts(results: list[dataclasses.MatchResult]) -> list[dict]:
+    return [result.to_dict() for result in results]
+
+
+def store_match_result(match_result: dataclasses.MatchResult) -> None:
+    """
+    Get the current match results, add the new result, and store the updated list
+    """
+    all_results = get_all_match_results()
+    all_results.append(match_result)
+
+    match_results_as_dicts = convert_match_results_to_dicts(all_results)
+    base.store_file(
+        file_path=settings_base.settings.MATCH_RESULTS_PATH,
+        file_name=settings_base.settings.MATCH_RESULTS_FILE,
+        contents=json.dumps(match_results_as_dicts),
+    )
