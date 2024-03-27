e46dd7e43015a046200a64818471ca4d8446f080
Aled Watkins <aled.watkins2@gmail.com>
2024-03-27 21:52:48 +0000

    Add `User.from_dict()`

--- squash_bot/core/data/user.py
+++ squash_bot/core/data/user.py
@@ -10,3 +10,11 @@ class User:
     @property
     def name(self) -> str:
         return self.global_name or self.username
+
+    @classmethod
+    def from_dict(cls, data: dict[str, str]) -> "User":
+        return User(
+            id=data["id"],
+            username=data["username"],
+            global_name=data["global_name"],
+        )
