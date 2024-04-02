c6d7223b63af40df3e16ec905b8b8d004f98d957
Aled Watkins <aled.watkins2@gmail.com>
2024-04-02 20:24:06 +0100

    Add `NoopFilterer`

--- squash_bot/match_tracker/filterers.py
+++ squash_bot/match_tracker/filterers.py
@@ -6,4 +6,10 @@ from squash_bot.match_tracker.data import dataclasses
 class Filterer(abc.ABC):
     @classmethod
     @abc.abstractmethod
-    def filter(self, matches: dataclasses.Matches) -> dataclasses.Matches: ...
+    def filter(cls, matches: dataclasses.Matches, **kwargs) -> dataclasses.Matches: ...
+
+
+class NoopFilterer(Filterer):
+    @classmethod
+    def filter(cls, matches: dataclasses.Matches, **kwargs) -> dataclasses.Matches:
+        return matches
