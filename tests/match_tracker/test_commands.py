850c19ef8a9c385c1888fa6ed76b816d299c690e
Aled Watkins <aled.watkins2@gmail.com>
2024-04-05 21:48:36 +0100

    Fix test

--- tests/match_tracker/test_commands.py
+++ tests/match_tracker/test_commands.py
@@ -258,7 +258,7 @@ class TestLeagueTable:
 
         content = response["data"]["content"]
         assert "global-user1 │      1 │        1" in content
-        assert "global-user2 │      1 │  " in content
+        assert "global-user2 │      1 │        1" in content
 
     def test_league_table_with_datetime_filter(self):
         user_one = core_dataclasses.User(id="1", username="user1", global_name="global-user1")
