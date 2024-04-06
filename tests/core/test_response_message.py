0b02879ced41dd48361a37802babb2b477004221
Luke Gregory <L.Gregory11@hotmail.co.uk>
2024-04-06 19:54:24 +0100

    Decorate ResponseBody with `attrs.frozen(kw_only=True)` and declare kw args in usage

--- tests/core/test_response_message.py
+++ tests/core/test_response_message.py
@@ -4,7 +4,9 @@ from squash_bot.core import response_message
 class TestResponseMessage:
     def test_channel_response_message_body(self):
         test_str = "This is a test"
-        channel_message_response_body = response_message.ChannelMessageResponseBody(test_str)
+        channel_message_response_body = response_message.ChannelMessageResponseBody(
+            content=test_str
+        )
         assert channel_message_response_body.as_dict() == {
             "type": 4,
             "data": {"content": test_str},
@@ -12,8 +14,8 @@ class TestResponseMessage:
 
     def test_ephemeral_channel_response_message_body(self):
         test_str = "This is another test"
-        ephemeral_channel_message_response_body = response_message.EphemeralChannelMessageResponseBody(
-            test_str
+        ephemeral_channel_message_response_body = (
+            response_message.EphemeralChannelMessageResponseBody(content=test_str)
         )
         assert ephemeral_channel_message_response_body.as_dict() == {
             "type": 4,
