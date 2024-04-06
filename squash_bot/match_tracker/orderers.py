fcc6477413523e45320b367f5d4ac6be24cc997f
Aled Watkins <aled.watkins2@gmail.com>
2024-04-06 16:12:01 +0100

    Add `ByMatchesField` orderer implementation
    This change adds an `Orderer` implementation that re-implements the
    logic already in the `ShowMatchesCommand` command.

--- squash_bot/match_tracker/orderers.py
+++ squash_bot/match_tracker/orderers.py
@@ -13,3 +13,16 @@ class NoopOrderer(Orderer):
     @classmethod
     def order(cls, matches: dataclasses.Matches, **kwargs) -> dataclasses.Matches:
         return matches
+
+
+class OptionalByMatchesField(Orderer):
+    @classmethod
+    def order(cls, matches: dataclasses.Matches, **kwargs) -> dataclasses.Matches:
+        field = kwargs.get("field")
+        if not field:
+            return matches
+
+        try:
+            return matches.sort_by(field)
+        except dataclasses.FieldDoesNotExist:
+            return matches
