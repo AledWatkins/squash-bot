5e0450918552c92ec1cadf0950601a2dc00d32de
Aled Watkins <aled.watkins2@gmail.com>
2024-04-06 16:59:11 +0100

    Change `_match_string` to put server first
    Prior to this change, we would always show the winner first, with the
    server marked with a `*`.
    
    This changes that so that the server is always first which removes the
    need for a special marker for the server.

--- tests/match_tracker/test_commands.py
+++ tests/match_tracker/test_commands.py
@@ -201,7 +201,7 @@ class TestShowMatches:
         content = response["data"]["content"]
         assert (
             content
-            == "```\n\nFriday, 1 January 2021:\n\tPaul!\t11 - 3\tJohn!*\n\nSaturday, 2 January 2021:\n\tPaul!\t11 - 3\tJohn!*```"
+            == "```\n\nFriday, 1 January 2021:\n\tJohn!\t3 - 11\tPaul!\n\nSaturday, 2 January 2021:\n\tJohn!\t3 - 11\tPaul!```"
         )
 
 
