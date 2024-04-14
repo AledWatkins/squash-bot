f963b12938bdd15f7ef3e42f1f97c3382e62e06c
Aled Watkins <aled.watkins2@gmail.com>
2024-04-14 18:40:11 +0100

    Add some badge definitions

---
+++ squash_bot/match_tracker/badges/badge_definitions.py
@@ -0,0 +1,343 @@
+from collections import defaultdict
+from decimal import Decimal
+
+import attrs
+
+from squash_bot.core.data import dataclasses as core_dataclasses
+from squash_bot.match_tracker.data import dataclasses as dataclasses
+
+from . import badge
+
+
+@attrs.frozen
+class Crush(badge.Badge):
+    """
+    A crush is a game where the winner won without the loser scoring any points.
+    """
+
+    @property
+    def display(self):
+        return f"{self.badge_earned_in.winner.name} crushed {self.badge_earned_in.loser.name}"
+
+
+class CrushCollector(badge.BadgeCollector[Crush]):
+    def __init__(self) -> None:
+        self._crushed_matches = set()
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        if match.loser_score == 0:
+            self._crushed_matches.add(match)
+
+    def collect(self) -> list[Crush]:
+        return [Crush(badge_earned_in=match_result) for match_result in self._crushed_matches]
+
+
+@attrs.frozen
+class CleanSweep(badge.Badge):
+    """
+    A clean sweep is where the player won all of their games.
+    """
+
+    player: core_dataclasses.User
+
+    @property
+    def display(self):
+        return f"{self.player.name} won all of their games"
+
+
+class CleanSweepCollector(badge.BadgeCollector[CleanSweep]):
+    def __init__(self) -> None:
+        # Track the last win so we can associate it to the badge
+        self._players_last_win = {}
+        self._players = set()
+        self._players_with_loss = set()
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        self._players.add(match.winner)
+        self._players_last_win[match.winner] = match
+        self._players_with_loss.add(match.loser)
+
+    def collect(self) -> list[CleanSweep]:
+        return [
+            CleanSweep(player=player, badge_earned_in=self._players_last_win[player])
+            for player in self._players - self._players_with_loss
+        ]
+
+
+@attrs.frozen
+class WoodenSpoon(badge.Badge):
+    """
+    A wooden spoon is where the player lost all of their games.
+    """
+
+    player: core_dataclasses.User
+
+    @property
+    def display(self):
+        return f"{self.player.name} lost all of their games"
+
+
+class WoodenSpoonCollector(badge.BadgeCollector[WoodenSpoon]):
+    def __init__(self) -> None:
+        # Track the last loss so we can associate it to the badge
+        self._players_last_loss = {}
+        self._players = set()
+        self._players_with_win = set()
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        self._players.add(match.loser)
+        self._players_last_loss[match.loser] = match
+        self._players_with_win.add(match.winner)
+
+    def collect(self) -> list[WoodenSpoon]:
+        return [
+            WoodenSpoon(player=player, badge_earned_in=self._players_last_loss[player])
+            for player in self._players - self._players_with_win
+        ]
+
+
+@attrs.frozen
+class WinStreak(badge.Badge):
+    """
+    A win streak is where the player has won a certain number of games in a row.
+    """
+
+    player: core_dataclasses.User
+    streak_length: int
+
+    @property
+    def display(self):
+        return f"{self.player.name} had a {self.streak_length} game win streak"
+
+
+class WinStreakCollector(badge.BadgeCollector[WinStreak]):
+    min_streak_length = 3
+
+    def __init__(self) -> None:
+        self._streaks = []
+        self._players_last_win = {}
+        self._current_streaks = defaultdict(int)
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        ended_streak = self._current_streaks[match.loser]
+        if ended_streak >= self.min_streak_length:
+            self._streaks.append(
+                WinStreak(
+                    player=match.loser,
+                    streak_length=ended_streak,
+                    badge_earned_in=self._players_last_win[match.loser],
+                )
+            )
+
+        self._current_streaks[match.loser] = 0
+        self._current_streaks[match.winner] += 1
+        self._players_last_win[match.winner] = match
+
+    def collect(self) -> list[WinStreak]:
+        # Collect any ongoing streaks
+        for player, streak_length in self._current_streaks.items():
+            if streak_length >= self.min_streak_length:
+                self._streaks.append(
+                    WinStreak(
+                        player=player,
+                        streak_length=streak_length,
+                        badge_earned_in=self._players_last_win[player],
+                    )
+                )
+
+        return self._streaks
+
+
+@attrs.frozen
+class LossStreak(badge.Badge):
+    """
+    A loss streak is where the player has lost a certain number of games in a row.
+    """
+
+    player: core_dataclasses.User
+    streak_length: int
+
+    @property
+    def display(self):
+        return f"{self.player.name} had a {self.streak_length} game loss streak"
+
+
+class LossStreakCollector(badge.BadgeCollector[LossStreak]):
+    min_streak_length = 3
+
+    def __init__(self) -> None:
+        self._streaks = []
+        self._players_last_loss = {}
+        self._current_streaks = defaultdict(int)
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        ended_streak = self._current_streaks[match.winner]
+        if ended_streak >= self.min_streak_length:
+            self._streaks.append(
+                LossStreak(
+                    player=match.winner,
+                    streak_length=ended_streak,
+                    badge_earned_in=self._players_last_loss[match.winner],
+                )
+            )
+
+        self._current_streaks[match.winner] = 0
+        self._current_streaks[match.loser] += 1
+        self._players_last_loss[match.loser] = match
+
+    def collect(self) -> list[WinStreak]:
+        # Collect any ongoing streaks
+        for player, streak_length in self._current_streaks.items():
+            if streak_length >= self.min_streak_length:
+                self._streaks.append(
+                    LossStreak(
+                        player=player,
+                        streak_length=streak_length,
+                        badge_earned_in=self._players_last_loss[player],
+                    )
+                )
+
+        return self._streaks
+
+
+@attrs.frozen
+class StreakBreaker(badge.Badge):
+    """
+    A streak breaker is where the player has broken another player's win streak.
+    """
+
+    player: core_dataclasses.User
+    opponent: core_dataclasses.User
+    streak_length: int
+
+    @property
+    def display(self):
+        return (
+            f"{self.player.name} broke {self.opponent.name}'s {self.streak_length} game win streak"
+        )
+
+
+class StreakBreakerCollector(badge.BadgeCollector[StreakBreaker]):
+    min_streak_length = 3
+
+    def __init__(self) -> None:
+        self._streaks = []
+        self._players_last_win = {}
+        self._current_streaks = defaultdict(int)
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        ended_streak = self._current_streaks[match.loser]
+        if ended_streak >= self.min_streak_length:
+            self._streaks.append(
+                StreakBreaker(
+                    player=match.winner,
+                    opponent=match.loser,
+                    streak_length=ended_streak,
+                    badge_earned_in=match,
+                )
+            )
+
+        self._current_streaks[match.loser] = 0
+        self._current_streaks[match.winner] += 1
+        self._players_last_win[match.winner] = match
+
+    def collect(self) -> list[StreakBreaker]:
+        return self._streaks
+
+
+@attrs.frozen
+class FirstWinAgainst(badge.Badge):
+    """
+    A first win against is where the player has won their first game against another player.
+    """
+
+    player: core_dataclasses.User
+    opponent: core_dataclasses.User
+
+    @property
+    def display(self):
+        return f"{self.player.name} won their first game against {self.opponent.name}"
+
+
+class FirstWinAgainstCollector(badge.BadgeCollector[FirstWinAgainst]):
+    def __init__(self) -> None:
+        self._first_wins = {}
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        winner_loser = (match.winner, match.loser)
+        if winner_loser not in self._first_wins:
+            self._first_wins[winner_loser] = match
+
+    def collect(self) -> list[FirstWinAgainst]:
+        return [
+            FirstWinAgainst(
+                player=winner_loser[0],
+                opponent=winner_loser[1],
+                badge_earned_in=match,
+            )
+            for winner_loser, match in self._first_wins.items()
+        ]
+
+
+@attrs.frozen
+class MVP(badge.Badge):
+    """
+    The MVP is the player who has the highest average point difference in a series of matches.
+    """
+
+    player: core_dataclasses.User
+    average_point_difference: Decimal
+
+    @property
+    def display(self):
+        return f"{self.player.name} had the highest average point difference of {self.average_point_difference:+}"
+
+
+class MVPCollector(badge.BadgeCollector[MVP]):
+    def __init__(self) -> None:
+        self._point_differences = defaultdict(int)
+        self._num_matches = defaultdict(int)
+        # We associate the badge with the last match played by the player
+        self._player_last_match = {}
+
+    def mutate_context_for_match(self, match: dataclasses.MatchResult) -> None:
+        point_difference = match.winner_score - match.loser_score
+        self._num_matches[match.winner] += 1
+        self._num_matches[match.loser] += 1
+
+        self._point_differences[match.winner] += point_difference
+        self._player_last_match[match.winner] = match
+
+        self._point_differences[match.loser] -= point_difference
+        self._player_last_match[match.loser] = match
+
+    def collect(self) -> list[MVP]:
+        highest_point_diff = 0
+        player_with_highest = None
+        for player in self._point_differences:
+            avg_point_diff = Decimal(
+                self._point_differences[player] / self._num_matches[player]
+            ).quantize(Decimal("0.01"))
+            if avg_point_diff > highest_point_diff:
+                highest_point_diff = avg_point_diff
+                player_with_highest = player
+
+        return [
+            MVP(
+                player=player_with_highest,
+                average_point_difference=highest_point_diff,
+                badge_earned_in=self._player_last_match[player_with_highest],
+            )
+        ]
+
+
+badge_collector_mapping: dict[type[badge.Badge], type[badge.BadgeCollector]] = {
+    Crush: CrushCollector,
+    CleanSweep: CleanSweepCollector,
+    WoodenSpoon: WoodenSpoonCollector,
+    WinStreak: WinStreakCollector,
+    LossStreak: LossStreakCollector,
+    StreakBreaker: StreakBreakerCollector,
+    FirstWinAgainst: FirstWinAgainstCollector,
+    MVP: MVPCollector,
+}
---
+++ tests/match_tracker/badges/test_badge_definitions.py
@@ -0,0 +1,209 @@
+from decimal import Decimal
+
+from squash_bot.match_tracker.badges import badge_definitions, queries
+from squash_bot.match_tracker.data import dataclasses
+
+from tests.factories import core as core_factories
+from tests.factories import match_tracker as match_tracker_factories
+
+
+class TestCrushCollector:
+    def test_finds_crush(self):
+        match_one = match_tracker_factories.MatchResultFactory(winner_score=11, loser_score=0)
+        match_two = match_tracker_factories.MatchResultFactory(winner_score=11, loser_score=1)
+        matches = dataclasses.Matches([match_one, match_two])
+
+        returned_badges = queries.collect_badges(matches=matches, badges=[badge_definitions.Crush])
+
+        assert len(returned_badges) == 1
+        assert returned_badges[0] == badge_definitions.Crush(badge_earned_in=match_one)
+
+
+class TestCleanSweepCollector:
+    def test_finds_clean_sweep(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        matches = dataclasses.Matches([match_one, match_two, match_three, match_four])
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.CleanSweep]
+        )
+
+        assert len(returned_badges) == 1
+        assert returned_badges[0] == badge_definitions.CleanSweep(
+            player=karl, badge_earned_in=match_three
+        )
+
+
+class TestWoodenSpoonCollector:
+    def test_finds_wooden_spoon(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        matches = dataclasses.Matches([match_one, match_two, match_three, match_four])
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.WoodenSpoon]
+        )
+
+        assert len(returned_badges) == 1
+        assert returned_badges[0] == badge_definitions.WoodenSpoon(
+            player=ricky, badge_earned_in=match_four
+        )
+
+
+class TestWinStreakCollector:
+    def test_finds_win_streak(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        # Create a match history where Karl wins three in a row and then loses
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_five = match_tracker_factories.MatchResultFactory(winner=karl, loser=ricky)
+        match_six = match_tracker_factories.MatchResultFactory(winner=steve, loser=karl)
+        matches = dataclasses.Matches(
+            [match_one, match_two, match_three, match_four, match_five, match_six]
+        )
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.WinStreak]
+        )
+
+        assert len(returned_badges) == 1
+        assert returned_badges[0] == badge_definitions.WinStreak(
+            player=karl, streak_length=3, badge_earned_in=match_five
+        )
+
+
+class TestLossStreakCollector:
+    def test_finds_loss_streak(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        # Create a match history where Karl wins three in a row and then loses
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_five = match_tracker_factories.MatchResultFactory(winner=karl, loser=ricky)
+        match_six = match_tracker_factories.MatchResultFactory(winner=steve, loser=karl)
+        matches = dataclasses.Matches(
+            [match_one, match_two, match_three, match_four, match_five, match_six]
+        )
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.LossStreak]
+        )
+
+        assert len(returned_badges) == 1
+        assert returned_badges[0] == badge_definitions.LossStreak(
+            player=ricky, streak_length=3, badge_earned_in=match_five
+        )
+
+
+class TestFirstWinAgainstCollector:
+    def test_finds_first_win_against(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=steve, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=karl)
+        matches = dataclasses.Matches([match_one, match_two, match_three, match_four])
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.FirstWinAgainst]
+        )
+
+        assert len(returned_badges) == 3
+        assert returned_badges == [
+            badge_definitions.FirstWinAgainst(
+                player=karl, opponent=steve, badge_earned_in=match_one
+            ),
+            badge_definitions.FirstWinAgainst(
+                player=steve, opponent=ricky, badge_earned_in=match_two
+            ),
+            badge_definitions.FirstWinAgainst(
+                player=steve, opponent=karl, badge_earned_in=match_four
+            ),
+        ]
+
+
+class TestStreakBreakerCollector:
+    def test_finds_broken_streaks(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        match_one = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_two = match_tracker_factories.MatchResultFactory(winner=karl, loser=ricky)
+        match_three = match_tracker_factories.MatchResultFactory(winner=karl, loser=steve)
+        match_four = match_tracker_factories.MatchResultFactory(winner=steve, loser=karl)
+        matches = dataclasses.Matches([match_one, match_two, match_three, match_four])
+
+        returned_badges = queries.collect_badges(
+            matches=matches, badges=[badge_definitions.StreakBreaker]
+        )
+
+        assert len(returned_badges) == 1
+        assert returned_badges == [
+            badge_definitions.StreakBreaker(
+                player=steve, opponent=karl, streak_length=3, badge_earned_in=match_four
+            ),
+        ]
+
+
+class TestMVP:
+    def test_finds_mvp(self):
+        ricky = core_factories.UserFactory(username="ricky")
+        steve = core_factories.UserFactory(username="steve")
+        karl = core_factories.UserFactory(username="karl")
+
+        match_one = match_tracker_factories.MatchResultFactory(
+            winner=karl, winner_score=11, loser=ricky, loser_score=9
+        )
+        match_two = match_tracker_factories.MatchResultFactory(
+            winner=steve, winner_score=11, loser=ricky, loser_score=0
+        )
+        match_three = match_tracker_factories.MatchResultFactory(
+            winner=karl, winner_score=11, loser=ricky, loser_score=9
+        )
+        match_four = match_tracker_factories.MatchResultFactory(
+            winner=karl, winner_score=11, loser=ricky, loser_score=9
+        )
+        match_five = match_tracker_factories.MatchResultFactory(
+            winner=ricky, winner_score=11, loser=steve, loser_score=0
+        )
+        matches = dataclasses.Matches([match_one, match_two, match_three, match_four, match_five])
+
+        returned_badges = queries.collect_badges(matches=matches, badges=[badge_definitions.MVP])
+
+        # Karl won 3 games with a 2 point lead so his average point difference is 2
+        # Steve won 1 game with an 11 point lead and lost 1 game by an 11 point difference so his average point difference is 0
+        # Ricky has a negative average point difference
+        assert len(returned_badges) == 1
+        assert returned_badges == [
+            badge_definitions.MVP(
+                player=karl,
+                average_point_difference=Decimal("2.00"),
+                badge_earned_in=match_four,
+            ),
+        ]
