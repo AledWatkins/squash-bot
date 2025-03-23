0c05ad58e50541a9e86d9a04fc47085478545881
Aled Watkins <aled.watkins2@gmail.com>
2025-03-23 21:00:39 +0000

    Use chatGPT to make scheduling message more human

--- scheduled_actions/actions/prompt_session_booking.py
+++ scheduled_actions/actions/prompt_session_booking.py
@@ -1,9 +1,12 @@
 import datetime
 import logging
-from collections.abc import Iterable
+from collections.abc import Collection, Iterable
+
+import attrs
 
 from common.settings import base as settings_base
 from common.timetable import timetable
+from common.vendors.openai import client as openai_client
 from scheduled_actions import action, dataclasses, send
 
 logger = logging.getLogger("scheduled_actions.actions")
@@ -12,6 +15,16 @@ logger.setLevel(logging.INFO)
 
 class PromptSessionBooking(action.Action):
     code = "prompt-session-booking"
+
+    _week_days = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday",
+    }
 
     def run(self, context: dict) -> None:
         sessions = self._get_sessions()
@@ -41,24 +54,99 @@ class PromptSessionBooking(action.Action):
         logger.info(f"{self.__class__.__name__} - Got {len(sessions)} sessions from API")
 
         # We only want to notify about sessions at 6pm on weekdays
-        time_of_day = [18]
+        time_of_day = [18, 19]
         days = timetable.DayType.WEEKDAY
         return timetable.CelticLeisureTimetable.filter_sessions(sessions, time_of_day, days=days)
 
     def _build_message_content(self, sessions: list[timetable.TimetableSession]) -> str:
+        # Use GPT to build the message content if the feature is enabled
+        # Otherwise, use the default method
+        # If we fail to build the content via GPT, we fall back to the default method
+        if settings_base.settings.feature_enabled(
+            settings_base.FlaggedFeature.USE_GPT_FOR_PROMPT_SESSION_BOOKING
+        ):
+            session_context = self.SessionsContext(
+                sessions=sessions,
+                preferred_time="6pm",
+                fallback_time="7pm",
+                notes=settings_base.settings.PROMPT_SESSION_BOOKING_GPT_NOTES,
+            )
+            try:
+                return self._build_content_via_openai(session_context)
+            except Exception as e:
+                print(e)
+                logger.error(f"{self.__class__.__name__} - Error building content via OpenAI: {e}")
+
+        return self._build_content(sessions)
+
+    @attrs.frozen
+    class SessionsContext:
+        sessions: Collection[timetable.TimetableSession]
+        preferred_time: str
+        fallback_time: str
+        notes: Collection[str]
+
+    def _build_content_via_openai(self, context: SessionsContext) -> str:
+        lines = [
+            f"You are helping a group of friends organise a weekly squash game. The current date is {datetime.date.today().isoformat()}. ",
+            "Here is a list of available court sessions for next week at their local gym:",
+        ]
+        sessions_by_day = {
+            "Monday": [],
+            "Tuesday": [],
+            "Wednesday": [],
+            "Thursday": [],
+            "Friday": [],
+        }
+        for session in context.sessions:
+            start = session.start_datetime
+            weekday = self._week_days[start.weekday()]
+            sessions_by_day[weekday].append(start)
+
+        # Add session data
+        for day, start_datetimes in sessions_by_day.items():
+            if start_datetimes:
+                lines.append(
+                    f"{day}: {', '.join([str(start.hour) + ':00' for start in start_datetimes])}"
+                )
+            else:
+                lines.append(f"{day}: No sessions available")
+
+        # Add preferred and fallback times
+        lines.append(
+            f"They play at {context.preferred_time}. Only mention {context.fallback_time} if there are very few options. "
+        )
+
+        # Add message guidelines
+        lines.append(
+            "Based on this information, provide the information that's required for the group to make a decision on when to play. "
+            f"Keep it short and to the point. Don't be cringe and over-the-top. Don't add information that's not relevant, e.g. if there are more than 1 day have {context.preferred_time} sessions, you don't need to mention {context.fallback_time}. "
+            "Only suggest sessions that are actually listed above. "
+        )
+
+        # Add notes
+        if context.notes:
+            lines.append("Some notes:")
+            lines.append(context.notes)
+            lines.append(
+                "Don't include these notes in the message, just use them to help you write it."
+            )
+
+        # Add example message
+        lines.append(
+            "An example of a message might be, 'Are we playing squash this week? Monday or Wednesday are free.' "
+            "Another example might be, 'Squash? We can do 6pm on Tuesday without [friend] or all of us on Monday at 7.' "
+        )
+
+        return openai_client.get_client().get_response("\n".join(lines))
+
+    def _build_content(self, sessions: list[timetable.TimetableSession]) -> str:
         base_msg = "Are we playing squash this week?"
 
-        week_days = {
-            0: "Monday",
-            1: "Tuesday",
-            2: "Wednesday",
-            3: "Thursday",
-            4: "Friday",
-            5: "Saturday",
-            6: "Sunday",
-        }
-        available_week_days = [week_days[session.start_datetime.weekday()] for session in sessions]
-        non_available_days = set(week_days.values()) - set(available_week_days)
+        available_week_days = list(
+            set(self._week_days[session.start_datetime.weekday()] for session in sessions)
+        )
+        non_available_days = set(self._week_days.values()) - set(available_week_days)
         if not available_week_days:
             return base_msg + " All 6pm sessions are already booked this week 💀"
         elif len(non_available_days) <= 2:
